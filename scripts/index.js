c7bef383804481688e050f79cd6cca07474b892c
charlestoriola <bugesey.em@gmail.com>
2021-01-06 15:26:51 +0100

    Modified carousel height

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+// var myVar;
+
+// 	function myFunction() {
+// 	  myVar = setTimeout(showPage, 6000);
+// 	}
+
+// 	function showPage() {
+// 	  document.getElementById("loader").style.display = "none";
+// 	  document.getElementById("maincontent").style.display = "block";
+// 	}
+
+var myIndex = 0;
+carousel2();
+
+function carousel2() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+    x[i].style.opacity = 0;  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  x[myIndex-1].style.opacity = 1; 
+  setTimeout(carousel2, 5000);    
+}
+
+
+$('#myCarousel').carousel({
+    interval:   1000
+  });
